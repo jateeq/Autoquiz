0e983602f88e2e841e43337050bc3d3d2c304f01
Jateeq <jawad.ateeq@gmail.com>
2017-04-23 17:44:20 -0400

    Stating a django project

---
+++ src/process.py
@@ -0,0 +1,14 @@
+'''
+'''
+
+def process(document):
+	# pre process document
+
+	# convert to bag of words
+
+	# find key concepts from the document
+
+	# convert the concepts into questions
+
+	# return question and answer pairs
+
